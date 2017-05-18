3fe11b4f82c0610177e8606fdac47fe6e59f3104
nieyh <nieyh@m15.cn>
2017-05-18 19:27:32 +0800

    创建kotlin项目

---
+++ app/src/main/java/me/example/nieyh/kotlintouchhelperdemo/KotlinActivity.java
@@ -0,0 +1,13 @@
+package me.example.nieyh.kotlintouchhelperdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KotlinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kotlin);
+    }
+}
